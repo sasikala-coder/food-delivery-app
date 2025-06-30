3a1114ff18e6cff50e31d5603ba44eb700d1ff7f
chandrayanapalli sasikala <chandrayanapallisasikala@gmail.com>
2025-06-30 14:09:20 +0530

    Update db.js

--- admin/backend/config/db.js
+++ admin/backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://utkarshanand:utkarshtomatoanand@cluster0.gr6lr.mongodb.net/food-del').then(()=>console.log("DB Connected"))
+    await mongoose.connect('mongodb+srv://chandrayanapallisasikala:tomatosasikala@cluster0.gr6lr.mongodb.net/food-del')
+        .then(()=>console.log("DB Connected"))
 }
